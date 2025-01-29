a33d9348b20f1646c930d6e42e23bde949e4ab54
kartik Jejurkar <kartikjejurkar@gmail.com>
2025-01-29 23:32:57 +0530

    git commit  -m"newupdate"

---
+++ multiblock.tf
@@ -0,0 +1,13 @@
+output myfirstblock  {
+    value = " This is my first output block "
+  
+}
+
+output mysecondblock {
+    value = " This is my second output block"
+  
+}
+
+output mythirdblock { 
+    value = "This is my third output block"
+}
---
+++ newvarible/variable.tf
@@ -0,0 +1,16 @@
+
+output "printuser" {
+
+    value = "my name is ${var.username}and age is ${var.age}"
+  
+}
+
+
+
+
+output "second" {
+  
+
+  
+    value = "user name is ${var.seconduser}"
+}
